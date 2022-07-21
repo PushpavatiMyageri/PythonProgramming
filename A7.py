d86d9636e8f5bda652c4a54bf62d2a3dd1509e86
PushpavatiMyageri <pushpam066@gmail.com>
2022-07-21 18:27:47 +0530

    commit

---
+++ A7.py
@@ -0,0 +1,8 @@
+#7.python program to swap two variables
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+temp = a
+a = b
+b = temp
+print("a value after swapping : ",a)
+print("b value after swapping : ",b)
