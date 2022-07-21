8ac6386de375df19b07bd0c419b68fbdb08e6fcd
PushpavatiMyageri <pushpam066@gmail.com>
2022-07-21 17:59:16 +0530

    commit

---
+++ A4.py
@@ -0,0 +1,6 @@
+#4.python program for simple interest
+p = float(input("enter the principle value : "))
+t = float(input("enter the time value : "))
+r = float(input("enter the rate value : "))
+simple_interest = (p*t*r)/100
+print(simple_interest)
