30787a3642b37859530f2b4f5926f644a65080c8
PushpavatiMyageri <pushpam066@gmail.com>
2022-07-22 18:10:42 +0530

    commit

---
+++ A11.py
@@ -0,0 +1,7 @@
+#11.python program to check if a number is odd or even
+def even_odd(n):
+    if(n%2==0):
+        print("the number is even :",n)
+    else:
+        print("the number is odd : ",n)
+even_odd(10)
