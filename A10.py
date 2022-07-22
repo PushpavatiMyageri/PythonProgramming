864c387f1f5cdf9fff62cec878a13ee192a3ee3d
PushpavatiMyageri <pushpam066@gmail.com>
2022-07-22 18:01:17 +0530

    commit

---
+++ A10.py
@@ -0,0 +1,11 @@
+#10. python program to check if a number is positive, negative or 0
+
+n = int(input("enter n value : "))
+if(n>0):
+    print("the number is positive : ",n)
+if(n<0):
+    print("the number is negative : ",n)
+if(n==0):
+    print("the number is zero : ",n)
+
+
