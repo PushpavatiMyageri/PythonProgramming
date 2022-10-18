a0cc6ed82c0f6866da64120ddd59378b1703d464
PushpavatiMyageri <pushpam066@gmail.com>
2022-10-18 09:47:22 +0530

    commit

---
+++ Assignment/Assignment_folder/module_3/module_4/module_6/Untitled-2.py
@@ -0,0 +1,4 @@
+a=int(input("enter the number of a"))
+b=int(input("enter the number of b"))
+c=a+b
+print(c)
---
+++ Assignment/Assignment_folder/module_3/module_4/module_6/tensor.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+a = tf.constant(10)
+b = tf.constant(20)
+tf.add(a,b),
+ 
+ 
