feef6a7f0f13377c59175c2c95d5e094d7750319
PushpavatiMyageri <pushpam066@gmail.com>
2022-11-06 13:55:14 +0530

    commit

--- Assignment/Assignment1/A1.py
+++ Assignment/Assignment1/A1.py
@@ -1,5 +1,10 @@
 #1.Python program to add two numbers
-a = int(input("Enter the value of a : "))
-b = int(input("Enter the value of b : "))
-sum = a + b
-print(sum)
+str1 = input("Enter the value of str1 : ")
+str2 = input("Enter the value of str2 : ")
+a = len(str1)
+b = len(str2)
+if a==b:
+    if sorted(str1)==sorted(str2):
+        print("anagram")
+    else:
+        print("not an anagram")
